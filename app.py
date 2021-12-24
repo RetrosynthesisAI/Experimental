645c537da7489d5f53dc43de6e217753d9116636
PoTRA-Package <retrosynthesis.ai@gmail.com>
2021-12-24 14:56:33 -0700

    Update

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app = MultiApp()
 st.image("media/logo.png")
 
 st.title('Global Disease Research')
-st.write('The aim of the Explore app is to enable scientists to explore molecular 3D visualizations as well as genomic disease trends.')
+st.write('The aim of the Explore app is to enable scientists to explore molecular 3D visualizations as well as global and population specific disease trends.')
 # Add all your application here
 app.add_app("Home", home.app)
 app.add_app("Molecular 3D", mol3d.app)
