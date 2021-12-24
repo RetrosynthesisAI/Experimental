35c092c21251cf6ddf0023aee385c0103780f511
PoTRA-Package <retrosynthesis.ai@gmail.com>
2021-12-24 14:40:50 -0700

    Update

--- apps/home.py
+++ apps/home.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def app():
-    st.title('Home')
+    st.title('Welcome')
 
-    st.write('This is the `home page` of this multi-page app.')
+    st.header('This is the `home page` of this multi-page app.')
 
     st.write('In this app, we will be building a simple classification model using the Iris dataset.')
