3278518c52336df05142dc9fa639cac84b5775d9
PoTRA-Package <retrosynthesis.ai@gmail.com>
2021-12-26 16:33:59 -0700

    Update

--- apps/chdis.py
+++ apps/chdis.py
@@ -2,16 +2,16 @@ import streamlit as st
 
 def app():
     st.subheader('Pediatric Disease Research in Egypt')
-    st.write('Recent Publications')
+    st.write('**Recent Publications**')
     st.write("[Large Cancer Survival Study/Children's Cancer Hospital Egypt] (https://onlinelibrary.wiley.com/doi/10.1002/ijc.33321)")
     st.write("[COVID19/Kawasaki Disease ](https://epag.springeropen.com/articles/10.1186/s43054-020-00029-9)")
     st.write("[CRISPR/Beta-Thalssemia IVS-1-110 mutation](https://stemcellres.biomedcentral.com/articles/10.1186/s13287-020-01876-4)")
     st.write("[Egyptian Sickle Cell Disease/IL-1β and IL-6 Polymorphisms](https://epag.springeropen.com/articles/10.1186/s43054-020-00025-z)")
     st.write("[Systemic Lupus Erythematosus/SNPs of SOD2 and GSTP1](https://www.sciencedirect.com/science/article/abs/pii/S0009912020309024?via%3Dihub)")
     st.write("[More studies](https://pubmed.ncbi.nlm.nih.gov/?term=egyptian+pediatric+disease&filter=articleattr.data&filter=years.2020-2021)")
-    st.write('Genomic Datasets')
-    st.write("**No central resource for Egyptian Pediatric Disease Genomic Datasets exists.**")
-    st.write('General Genomic Resources')
+    st.write('**Genomic Datasets**')
+    st.write("No central resource for Egyptian Pediatric Disease Genomic Datasets exists.")
+    st.write('**General Genomic Resources**')
     st.write("[Egyptian and North African Reference Genome](https://www.egyptian-genome.org/)")
     st.write("[Interactive Population Genome 3D](https://www.egyptian-genome.org/img/PCA_interactive.html)")
  
