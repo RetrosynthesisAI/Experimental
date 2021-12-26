aa51227397df51d06d291f10b7400719970fe73f
PoTRA-Package <retrosynthesis.ai@gmail.com>
2021-12-26 15:52:39 -0700

    Update

--- apps/chdis.py
+++ apps/chdis.py
@@ -3,6 +3,6 @@ import streamlit as st
 def app():
     st.subheader('Pediatric Disease Research in Egypt')
     st.write("[COVID19/Kawasaki Disease ](https://epag.springeropen.com/articles/10.1186/s43054-020-00029-9)")
-    st.write(' ')
+    st.write("[CRISPR/Beta-Thalssemia IVS-1-110 mutation](https://stemcellres.biomedcentral.com/articles/10.1186/s13287-020-01876-4)")
     st.write(' ')
  
