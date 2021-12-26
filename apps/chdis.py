671f6ff32334097223c5639109784580cff6e356
PoTRA-Package <retrosynthesis.ai@gmail.com>
2021-12-26 15:42:43 -0700

    Update

--- apps/chdis.py
+++ apps/chdis.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def app():
     st.subheader('Pediatric Disease Research in Egypt')
-    st.write("COVID19/Kawasaki Disease [link](https://epag.springeropen.com/articles/10.1186/s43054-020-00029-9#availability-of-data-and-materials)")
+    st.write("[COVID19/Kawasaki Disease ](https://epag.springeropen.com/articles/10.1186/s43054-020-00029-9)")
     st.write(' ')
     st.write(' ')
  
