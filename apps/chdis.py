730a98d4ef4067d98e9125a964fbce3d435c933b
PoTRA-Package <retrosynthesis.ai@gmail.com>
2021-12-26 16:03:55 -0700

    Update

--- apps/chdis.py
+++ apps/chdis.py
@@ -5,5 +5,8 @@ def app():
     st.write('Recent Publications')
     st.write("[COVID19/Kawasaki Disease ](https://epag.springeropen.com/articles/10.1186/s43054-020-00029-9)")
     st.write("[CRISPR/Beta-Thalssemia IVS-1-110 mutation](https://stemcellres.biomedcentral.com/articles/10.1186/s13287-020-01876-4)")
-    st.write('Genomic Datasets')
+    st.write("[Egyptian Sickle Cell Disease/IL-1β and IL-6 Polymorphisms](https://epag.springeropen.com/articles/10.1186/s43054-020-00025-z )")
+    st.write('Genomic Resources')
+    st.write('Egyptian Genome Reference')
+    st.write('Interactive Population Genome 3D')
  
