a3b1ea1d7f9bbd8082d2af26970b2cd87b95d562
PoTRA-Package <retrosynthesis.ai@gmail.com>
2021-12-26 15:40:36 -0700

    Update

--- apps/chdis.py
+++ apps/chdis.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def app():
     st.subheader('Pediatric Disease Research in Egypt')
-    st.write('COVID19/Kawasaki Disease [link](Unleashing the mysterious link between COVID-19 and a famous childhood vasculitis: Kawasaki disease)')
+    st.write("COVID19/Kawasaki Disease [link](https://epag.springeropen.com/articles/10.1186/s43054-020-00029-9#availability-of-data-and-materials)")
     st.write(' ')
     st.write(' ')
  
