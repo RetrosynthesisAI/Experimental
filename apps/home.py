05c1bf00dfc22833ce2d807408bacf3a9bf4abca
PoTRA-Package <retrosynthesis.ai@gmail.com>
2021-12-26 15:16:01 -0700

    Update

--- apps/home.py
+++ apps/home.py
@@ -1,5 +1,7 @@
 import streamlit as st
+from PIL import Image
 
 def app():
-    st.subheader('Country Spotlight')
+    st.subheader('Regional Spotlight')
+    st.image("media/mideast.jpeg")
     st.write(' ')
