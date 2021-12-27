e1e5f64fff236e09c945721b07393541cb10a749
PoTRA-Package <retrosynthesis.ai@gmail.com>
2021-12-26 17:34:40 -0700

    Update

--- apps/drugres.py
+++ apps/drugres.py
@@ -1,7 +1,21 @@
 import streamlit as st
 
 def app():
-    st.subheader('Drug Resistant Diseases')
-    st.write('Coming soon.')
-
+    st.subheader('Drug Resistant Diseases in Cyprus, Iran and Morocco')
+    st.write('**Recent Publications**')
+    st.write("[Antimicrobial Resistance in Bacteria/Mechanisms, Evolution, and Persistence] (https://link.springer.com/article/10.1007%2Fs00239-019-09914-3)")
+    st.write("[Antibiotic Resistance/Transposons Tn6608 and Tn6609](https://www.sciencedirect.com/science/article/pii/S2213716521001867?via%3Dihub)")
+    st.write("[Cancer Drug Resistance/Role of Noncoding RNAs and Sirtuins](https://www.sciencedirect.com/science/article/abs/pii/S0014299920301862?via%3Dihub)")
+    st.write("[Genomic Mutations Mediating Drug Resistant TB/Rifampicin in Northern Morocco](https://www.scielo.br/j/rimtsp/a/bxXyCv9MQHjY5vfdcTgyRQg/?lang=en)")
+    st.write("[HIV-1 Genotypic Drug Resistance Assay/Group M HIV-1 strains](https://www.sciencedirect.com/science/article/pii/S1567134820300757?via%3Dihub)")
+    st.write("[3D-QSAR Molecular Docking/Explore Novel DHFR Inhibitors] (https://pubmed.ncbi.nlm.nih.gov/34825630/)")
+    st.write("[More studies](https://pubmed.ncbi.nlm.nih.gov/?term=egyptian+pediatric+disease&filter=articleattr.data&filter=years.2020-2021)")
+    st.write('**Genomic Datasets**')
+    st.write("No central resource for Middle-Eastern Drug Resistant Genomic Datasets exists.")
+    st.write('**General Genomic Resources**')
+    st.write("[CY-BioBank](https://biobank.cy/)")
+    st.write("[Iranome](http://www.iranome.ir/)")
+    st.write("[Moroccan Genetic Disease Database](http://mgdd.pasteur.ma/)")
+    st.write("[Resistance Map](https://resistancemap.cddep.org/index.php)")
+     
  
