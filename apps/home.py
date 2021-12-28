aff1b528b2bc117823245ea6c5044d92abd51dfb
PoTRA-Package <retrosynthesis.ai@gmail.com>
2021-12-28 12:55:17 -0700

    Update

--- apps/home.py
+++ apps/home.py
@@ -9,5 +9,5 @@ def app():
     st.write(' ')
     iris = load_iris()
     df = iris.data
-    fig = px.scatter_3d(df, x=df['0'], y=df['1'], z=df['2'], color=df['3'])
+    fig = scatter_3d(df, x=df['0'], y=df['1'], z=df['2'], color=df['3'])
     fig.show()
