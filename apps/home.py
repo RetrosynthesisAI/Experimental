a37109bb8cfc9dc5fb9ce182b71faf1ee1d79802
PoTRA-Package <retrosynthesis.ai@gmail.com>
2021-12-28 12:44:57 -0700

    Update

--- apps/home.py
+++ apps/home.py
@@ -6,6 +6,6 @@ def app():
     st.subheader('Regional Focus')
     st.image("media/mideast.jpeg")
     st.write(' ')
-    df = data.iris()
-    fig = scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width', color='species')
+    df = st.data.iris()
+    fig = st.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width', color='species')
     st.plotly_chart(fig)
