91b864a181c64b49368c0d23a3ed658915286cc3
PoTRA-Package <retrosynthesis.ai@gmail.com>
2021-12-28 12:29:13 -0700

    Update

--- apps/home.py
+++ apps/home.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from PIL import Image
+import plotly.express as px
 
 def app():
     st.subheader('Regional Focus')
     st.image("media/mideast.jpeg")
     st.write(' ')
+    df = px.data.iris()
+    fig = px.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width', color='species')
+    fig.show()
