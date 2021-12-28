e615268a1a9db7963e3956e9f7c863ed1b4385f0
PoTRA-Package <retrosynthesis.ai@gmail.com>
2021-12-27 17:26:05 -0700

    Update

--- apps/zoo.py
+++ apps/zoo.py
@@ -4,7 +4,7 @@ def app():
     st.subheader('Zoonotic Diseases in Algeria, Kuwait, Jordan and Oman')
     st.write("Recent Publications")
     st.write("[Detection System of Infectious Diseases/Animal Health Status in Kuwait] (https://www.frontiersin.org/articles/10.3389/fvets.2021.676661/full)")
-     st.write("[Limited Genetic Diversity in Middle East Respiratory Syndrome/COVID Variants in Dromedary Camels in Jordan] (https://www.mdpi.com/1999-4915/13/4/592)")
+    st.write("[Limited Genetic Diversity in Middle East Respiratory Syndrome/COVID Variants in Dromedary Camels in Jordan] (https://www.mdpi.com/1999-4915/13/4/592)")
     st.write("[Middle East Respiratory Syndrome] (https://www.thelancet.com/journals/lancet/article/PIIS0140-6736(19)33221-0/fulltext )")
     st.write("[Genetically 'pure' Fasciola Gigantica Discovered in Algeria/Spreading Risk into North-Western Maghreb Countries] (https://onlinelibrary.wiley.com/doi/10.1111/tbed.13572)")
     st.write("[Zoonotic Diseases in Oman: Successes, Challenges, and Future Directions] (https://www.liebertpub.com/doi/10.1089/vbz.2019.2458?url_ver=Z39.88-2003&rfr_id=ori:rid:crossref.org&rfr_dat=cr_pub%20%200pubmed)")
